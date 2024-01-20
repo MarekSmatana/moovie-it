88048c25a40da484c6031570297ff1dc8988a06c
MarekSmatana <smat.marek.dev@gmail.com>
2024-01-21 00:56:38 +0100

    remove from favorites

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -1,17 +1,20 @@
-import { Box, HStack, Image } from "@chakra-ui/react"
+import { Box, HStack, IconButton, Image } from "@chakra-ui/react"
 import { MovieShort } from "../types"
 import { Link } from "react-router-dom"
+import { CloseIcon } from "@chakra-ui/icons"
 
 type MoviesListProps = {
   data: MovieShort[]
+  onMovieRemove?: (movie: MovieShort) => void
 }
 
-export default function MoviesList({ data }: MoviesListProps) {
+export default function MoviesList({ data, onMovieRemove }: MoviesListProps) {
   return (
     <HStack wrap="wrap" justify="center" mt={4}>
       {data.map((item) => (
         <Link key={item.imdbID} to={`/detail/${item.imdbID}`}>
           <Box
+            position="relative"
             p={1}
             border={1}
             borderColor="transparent"
@@ -35,6 +38,23 @@ export default function MoviesList({ data }: MoviesListProps) {
                 transform: "scale(1.2)",
               }}
             />
+            {onMovieRemove && (
+              <IconButton
+                variant="ghost"
+                icon={<CloseIcon />}
+                aria-label="remove"
+                position="absolute"
+                top={0}
+                right={0}
+                _hover={{
+                  color: "red.500",
+                }}
+                onClick={(e) => {
+                  e.preventDefault()
+                  onMovieRemove(item)
+                }}
+              />
+            )}
           </Box>
         </Link>
       ))}
--- src/routes/favorites.tsx
+++ src/routes/favorites.tsx
@@ -1,9 +1,18 @@
 import { Box } from "@chakra-ui/react"
 import MoviesList from "../components/MoviesList"
-import { getFavorites } from "../services/favoriteService"
+import * as favoriteService from "../services/favoriteService"
+import React from "react"
+import { MovieShort } from "../types"
 
 export default function FavoritesRoute() {
-  const favorites = getFavorites()
+  const [favorites, setFavorites] = React.useState(
+    favoriteService.getFavorites()
+  )
+
+  const handleRemoveFavorite = (movie: MovieShort) => {
+    setFavorites((prev) => prev.filter((f) => f.imdbID !== movie.imdbID))
+    favoriteService.setFavorite(movie, false)
+  }
 
   return favorites.length === 0 ? (
     <Box
@@ -16,6 +25,6 @@ export default function FavoritesRoute() {
       No favorite movies.
     </Box>
   ) : (
-    <MoviesList data={favorites} />
+    <MoviesList data={favorites} onMovieRemove={handleRemoveFavorite} />
   )
 }
